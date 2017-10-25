a34f7a2020dce6a10919780479ef2b51680e353a
areefhida <areef.hiide@gmail.com>
2017-10-25 16:09:57 +0700

    add navigation

--- OneStop/OneStop/ViewModels/MasterPageViewModel.cs
+++ OneStop/OneStop/ViewModels/MasterPageViewModel.cs
@@ -5,17 +5,23 @@ using OneStop.Views;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace OneStop.ViewModels
 {
-    public class MasterPageViewModel : BindableBase
+    public class MasterPageViewModel : ViewModelBase
     {
 
         private List<MasterItem> _listMenu;
         IAuthService _authservice;
+        IPageDialogService _dialogService;
+        INavigationService _navigationService;
+
+        public DelegateCommand<string> OnItemTappedCommand { get; set; }
 
         public List<MasterItem> listMenu
         {
@@ -23,11 +29,20 @@ namespace OneStop.ViewModels
             set { _listMenu = value; }
         }
 
-        public MasterPageViewModel(IAuthService authservice)
+        public MasterPageViewModel(IAuthService authservice, IPageDialogService dialogService,
+            INavigationService navigationService)
         {
             _authservice = authservice;
+            _dialogService = dialogService;
+            _navigationService = navigationService;
             var isLogin = !string.IsNullOrEmpty(Settings.access_token);
             _listMenu = _authservice.GetMenus(isLogin);
+            OnItemTappedCommand = new DelegateCommand<string>(Tapped);
+        }
+
+        private void Tapped(string navigation)
+        {
+            _navigationService.NavigateAsync(navigation);
         }
     }
 }
